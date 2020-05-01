7d6ce7e2d78aec9d333ad062c4c903ebcb0536d7
linhletd <linhletd@gmail.com>
2020-05-01 14:44:15 +0700

    add and change to static method of class ConvertFile

--- utils/ascii_utf8.js
+++ utils/ascii_utf8.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-const EventEmitter = require('events');
+const Transform  = require('stream').Transform;
 
-module.exports = class ConvertFile{
+class ConvertFile{
     constructor(originFileName, force = false, bufSize = 2){
         this.file = `${process.cwd()}/data/${originFileName}`;
         this.bufSize = bufSize;
@@ -10,14 +10,14 @@ module.exports = class ConvertFile{
         this.totalByte;
         this.pos = 0;
         this.pos1 = 0;
-        this.posn;
-        this.posu;
-        this.a;
-        this.u = {val: undefined, count: 0};
-
     }
+    static pos = 0;
+    static posn;
+    static posu;
+    static a;
+    static u = {val: undefined, count: 0};
 
-    utf8tize(buf,pos){
+    static utf8tize(buf){
         let arr = [];
         buf.map((val,idx) =>{
             if(this.posn && val){
@@ -25,20 +25,49 @@ module.exports = class ConvertFile{
             }
             else if(val < 128){
                 arr.push(val);
-                val === 0 && (this.posn = pos + idx +1)
+                val === 0 && (this.posn = this.pos + idx +1)
             }
             else{
                 let _val = val.toString(2);
                 let b1 = Number.parseInt('110000' + _val.slice(0,2),2);
                 let b2 = Number.parseInt('10' + _val.slice(2),2);
                 arr.push(b1,b2);
-                pos + idx === this.posu && (_val.slice(0,2) != '10' ? this.a = true : this.u.val = true, this.u.count++);
+                this.pos + idx === this.posu && (_val.slice(0,2) != '10' ? this.a = true : this.u.val = true, this.u.count++);
                 _val.slice(0,3) === '110' && (this.posu = this.pos + idx + 1)
             }
     
         })
+        this.pos += buf.length;
         return Buffer.from(arr)
     }
+
+    static convert(file){
+        let temp = `${process.cwd()}/temp/${file}_temp`
+        let readStream = fs.createReadStream(`${process.cwd()}/data/${file}`);
+        let writeStream = fs.createWriteStream(`${process.cwd()}/temp/${file}_temp`);
+        let f = this.utf8tize.bind(this);
+        let utf8tizer = new Transform({
+            transform: function(chunk, encoding, callback) {
+                let newChunk;
+                try {
+                    newChunk = f(chunk)
+                }
+                catch(e){
+                    console.log('error occurs: ', e.message);
+                    this.end();
+                    fs.unlink(temp,()=>{})
+                    return
+                }
+                this.push(newChunk);
+                callback();
+            }
+        });
+        readStream.pipe(utf8tizer).pipe(writeStream);
+        writeStream.on('close',()=>{
+            console.log('complete')
+        })
+    }
+
 
     copy(fd, fd1){
         let r,_r = 0, w = 0;
@@ -65,7 +94,7 @@ module.exports = class ConvertFile{
                     };
                     _r++;
                     try{
-                        var buf1 = this.utf8tize(buf,pos);
+                        var buf1 = ConvertFile.utf8tize(buf);
                     }
                     catch(err){
                         reject(err);
@@ -111,7 +140,7 @@ module.exports = class ConvertFile{
         Promise.all([p1,p2,p3])
         .then(([fd,fd1,size]) => {
             return this.copy(fd, fd1).then(() =>{
-                if(this.a || !this.u.val){
+                if(ConvertFile.a || !ConvertFile.u.val){
                     console.log('the source file is absolutely ascii format');
                     fs.unlink(this.file,()=>{
                         fs.rename(this.tempFile, this.file,()=>{
@@ -119,8 +148,8 @@ module.exports = class ConvertFile{
                         })
                     })
                 }
-                else if(this.u.val){
-                    console.log(`the source file seems to be utf8 format already, at least ${this.u.count} pattern(s) found.\nif you want to force to convert, set 'force' parameter to true then run again `);
+                else if(ConvertFile.u.val){
+                    console.log(`the source file seems to be utf8 format already, at least ${ConvertFile.u.count} pattern(s) found.\nif you want to force to convert, set 'force' parameter to true then run again.`);
                     if(this.force){
                         fs.unlink(this.file,()=>{
                             fs.rename(this.tempFile, this.file,()=>{
@@ -152,3 +181,4 @@ module.exports = class ConvertFile{
     
     }
 }
+module.exports = ConvertFile
