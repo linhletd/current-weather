1986c4b5c8ce0886ecb1c8a13b6ab2b8b71a9234
linhletd <linhletd@gmail.com>
2020-05-10 23:24:44 +0700

    fix undefined this bug

--- helpers/similarity.js
+++ helpers/similarity.js
@@ -3,6 +3,7 @@ class Similarity {
     this.src;
     this.des;
     this.arr;
+    this.generate = this.generate.bind(this)
   }
   findMin(...args){
     return args.reduce((acc, cur) => {
@@ -26,7 +27,7 @@ class Similarity {
       }
       let i1 = i - 1 >= 0 ? i - 1 : i;
       let j1 = j - 1 >= 0 ? j - 1 : j;
-      let min = findMin(this.arr[i][j1], this.arr[i1][j1], this.arr[i1][j]);
+      let min = this.findMin(this.arr[i][j1], this.arr[i1][j1], this.arr[i1][j]);
         if( this.arr[i1][j1] === min){
           i = i1;
           j = j1;
@@ -55,7 +56,7 @@ class Similarity {
   }
 
   same(i,j){
-    if(a[i] !== b[j]){
+    if(this.des[i] !== this.src[j]){
       return false;
     }
     let countai = this.des.slice(0, i+1).filter((cur) => (cur == this.des[i])).length;
@@ -65,7 +66,7 @@ class Similarity {
         return true;
       }
       else if(countbj > countai){
-        let x = i -1 >= 0 ? optimizePath(i-1, j): optimizePath(i, j-1) ;
+        let x = i -1 >= 0 ? this.optimizePath(i-1, j): this.optimizePath(i, j-1) ;
         
         let check = x.filter((cur) =>{
           if(cur[2] === 0 && cur[0] === i){
@@ -91,7 +92,6 @@ class Similarity {
     let m = a.length;
     let n = b.length;
     let arr = [];
-
     this.des = a;
     this.src = b;
     this.arr = arr;
@@ -102,7 +102,7 @@ class Similarity {
     for(let i = 0; i < m; i++){
       for(let j = 0; j < n; j++){
         if(i == 0){
-           if(same(i,j)){
+           if(this.same(i,j)){
             arr[i][j] = arr[i][j-1]||0;
           }
           else{
@@ -110,18 +110,18 @@ class Similarity {
           }
         }
         else {
-          if(same(i,j)){
-            arr[i][j] = findMin(arr[i][j-1], arr[i-1][j-1], arr[i-1][j] );
+          if(this.same(i,j)){
+            arr[i][j] = this.findMin(arr[i][j-1], arr[i-1][j-1], arr[i-1][j] );
           }
           else {
-            arr[i][j] = findMin(arr[i][j-1], arr[i-1][j-1], arr[i-1][j] ) + 1;
+            arr[i][j] = this.findMin(arr[i][j-1], arr[i-1][j-1], arr[i-1][j] ) + 1;
           }
         }
     
       }
     }
 
-    let optpath = optimizePath();
+    let optpath = this.optimizePath();
     let aIndx = [];
     let bIndx = [];
     let path = optpath.map((cur) =>{
